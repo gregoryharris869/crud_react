0addb42ae12f8ec754c2ac833cd221bf26d4d8d8
Gregory Harris <63122765+gregoryharris869@users.noreply.github.com>
2024-08-29 13:48:19 -0500

    Lesson 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,29 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 // custom components
-import CustomForm from './components/CustomForm'
-import TaskList from './components/TaskList';
+import CustomForm from "./components/CustomForm";
+import TaskList from "./components/TaskList";
 
 function App() {
   const [tasks, setTasks] = useState([]);
 
   const addTask = (task) => {
-    setTasks(prevState => [...prevState, task])
-  }
+    setTasks((prevState) => [...prevState, task]);
+  };
+
+  const deleteTask = (id) => {
+    setTasks((prevState) => prevState.filter((t) => t.id !== id));
+  };
 
   return (
     <div className="container">
       <header>
         <h1>My Task List</h1>
       </header>
-      <CustomForm addTask={addTask}/>
-      {tasks && <TaskList tasks={tasks}/>}
+      <CustomForm addTask={addTask} />
+      {tasks && <TaskList tasks={tasks} deleteTask={deleteTask} />}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
